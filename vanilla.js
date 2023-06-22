379a710821318a5e1435edb8f83dbe68634016fb
GabrielVaze <111015130+GabrielVaze@users.noreply.github.com>
2023-06-22 00:29:12 -0300

    fim
    a

--- vanilla.js
+++ vanilla.js
@@ -1,12 +1,10 @@
+
+// começo das skill
+
+
 var VanillaTilt = (function () {
     'use strict';
-    
-    /**
-     * Created by Sergiu Șandor (micku7zu) on 1/27/2017.
-     * Original idea: https://github.com/gijsroge/tilt.js
-     * MIT License.
-     * Version 1.8.0
-     */
+  
     
     class VanillaTilt {
       constructor(element, settings = {}) {
@@ -497,7 +495,7 @@ var VanillaTilt = (function () {
     
     }());
 
-
+// fim das skill
 
 
 
